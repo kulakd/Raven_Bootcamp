dfe1b91a0f5d16b99451d8e46f147a97013f7ac2
David <dkulakowski1999@gmail.com>
2023-04-11 18:25:31 +0200

    Add project files.

---
+++ DocumentStoreHolder.cs
@@ -0,0 +1,27 @@
+﻿using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven
+{
+        public static class DocumentStoreHolder
+        {
+            private static readonly Lazy<IDocumentStore> LazyStore =
+                new Lazy<IDocumentStore>(() =>
+                {
+                    var store = new DocumentStore
+                    {
+                        Urls = new[] { "http://localhost:8080" },
+                        Database = "Northwind"
+                    };
+
+                    return store.Initialize();
+                });
+
+            public static IDocumentStore Store =>
+                LazyStore.Value;
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Raven.Client.Documents;
+using System.Diagnostics;
+
+namespace Raven
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var p1 = session.Load<Product>("products/1-A");
+                var p2 = session.Load<Product>("products/1-A");
+                Debug.Assert(ReferenceEquals(p1, p2));
+            }
+            System.Console.WriteLine("Press <ENTER> to continue...");
+            System.Console.ReadLine();
+        }
+    }
+}
