cf7c5834ff769c7d67ae5f7e4c720350b146600a
David <dkulakowski1999@gmail.com>
2023-04-12 08:20:55 +0200

    Dodanie nowego unitu

---
+++ Raven_5/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven_5
+{
+    public class Order
+    {
+        public string Id { get; set; }
+        public string Company { get; set; }
+        public DateTimeOffset OrderedAt { get; set; }
+    }
+}
---
+++ Raven_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Documents;
+
+namespace Raven_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var ordersIds = (
+                    from order in session.Query<Order>()
+                    where order.Company == "companies/1-A"
+                    orderby order.OrderedAt
+                    select order.Id
+                    ).ToList();
+
+                foreach (var id in ordersIds)
+                {
+                    Console.WriteLine(id);
+                }
+            }
+        }
+    }
+
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] { "http://localhost:8080" },
+                    Database = "Northwind"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store =>
+            LazyStore.Value;
+    }
+}
